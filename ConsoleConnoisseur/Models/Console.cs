311de064de143bf8623476b1752cca37173efaa8
ross.macnamara535 <ross.macnamara535@gmail.com>
2019-06-10 15:23:58 +0100

    Added Console.cs, ConsoleServices.cs and IConsoleRepository.cs

---
+++ ConsoleConnoisseur/Models/Console.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleConnoisseur.Models
+{
+    public class Console
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public int Released { get; set; }
+        public string About { get; set; }
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; }
+        public string ImgThumbnailUrl { get; set; }
+    }
+}
---
+++ ConsoleConnoisseur/Models/IConsoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleConnoisseur.Models
+{
+    interface IConsoleRepository
+    {
+        IEnumerable<Console> GetAllConsoles();
+        Console GetConsoleById(int consoleId);
+    }
+}
