4dff12d7f54d6d23dccbd106fe8441ab29062306
ross.macnamara535 <ross.macnamara535@gmail.com>
2019-06-11 15:23:29 +0100

    DbContext and DBConsoleService classes added and called in start up created connection string

---
+++ ConsoleConnoisseur/Models/DBConsoleService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleConnoisseur.Models
+{
+    public class DBConsoleService : IConsoleRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DBConsoleService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Console> GetAllConsoles()
+        {
+            return _appDbContext.Consoles;
+        }
+
+        public Console GetConsoleById(int consoleId)
+        {
+            return _appDbContext.Consoles.FirstOrDefault(c => c.Id == consoleId);
+        }
+    }
+}
--- ConsoleConnoisseur/Startup.cs
+++ ConsoleConnoisseur/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -34,7 +35,10 @@ namespace ConsoleConnoisseur
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddTransient<IConsoleRepository, ConsoleServices>();
+            services.AddTransient<IConsoleRepository, DBConsoleService>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
